18271086065db32f614527fffd6d126aa44118e4
James Montemagno <james.montemagno@gmail.com>
2022-11-17 15:42:33 -0800

    Add project files.

---
+++ XFDataTypeSample/XFDataTypeSample/MainViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CommunityToolkit.Mvvm;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace XFDataTypeSample
+{
+    public partial class MainViewModel : ObservableObject
+    {
+
+        [ObservableProperty]
+        string display;
+
+        int count;
+        [RelayCommand]
+        void Increase()
+        {
+            count++;
+            Display = $"Count: {count}";
+        }
+    }
+}
